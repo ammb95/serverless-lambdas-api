afbdfb6691031d8555f4a5c5a023af7caeb588e5
Arthur Bortolini <arthur_bortolini@epam.com>
2023-10-13 22:43:25 +0200

    getProductById dynamo db integration

--- products-service/src/functions/get-product-by-id/handler.ts
+++ products-service/src/functions/get-product-by-id/handler.ts
@@ -1,25 +1,55 @@
+import AWS from 'aws-sdk';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import products from '@libs/data.json';
 import schema from './schema';
-
-type Product = {
-  id: string;
-  title: string;
-  description: string;
-  price: number;
-  img: string;
-};
-
-const findProduct = (id: string) => (product: Product) => product.id === id;
 
 const getProductById: ValidatedEventAPIGatewayProxyEvent<
   typeof schema
 > = async event => {
-  const product =
-    products.find(findProduct(event.pathParameters.id)) ?? ({} as Product);
-  return formatJSONResponse(product);
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  try {
+    const params = {
+      TableName: process.env.PRODUCTS_TABLE,
+      KeyConditionExpression: 'Id = :id',
+      ExpressionAttributeValues: {
+        ':id': event.pathParameters.id
+      }
+    };
+
+    const result = await dynamodb.query(params).promise();
+
+    const product = result.Items[0];
+
+    if (!product) {
+      return formatJSONResponse({
+        error: 'Product not found',
+        status: 404
+      });
+    }
+
+    const stockParams = {
+      TableName: process.env.STOCKS_TABLE,
+      KeyConditionExpression: 'ProductId = :productId',
+      ExpressionAttributeValues: {
+        ':productId': product.Id
+      }
+    };
+
+    const stockResult = await dynamodb.query(stockParams).promise();
+
+    product.count = stockResult.Items[0].Count;
+
+    return formatJSONResponse({
+      product
+    });
+  } catch (error) {
+    return formatJSONResponse({
+      error: 'Failed to retrieve product',
+      status: 500
+    });
+  }
 };
 
 export const main = middyfy(getProductById);
